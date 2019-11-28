eb51fb9ac425ea9f0719f28b7ba595b96f6e0469
Yesi Days <silvercorp@gmail.com>
2019-11-27 22:58:10 -0600

    Conceptos básicos

---
+++ conceptos-basicos.r
@@ -0,0 +1,98 @@
+#Conceptos básicos - https://youtu.be/4sBZjfTTv8I
+#Twitter: @silvercorp    Facebook: yesidays
+
+5+3
+5+3
+100 + 5
+15.39 + 12.1
+3-1
+5 -10
+5- 10
+5-10
+5 - 10
+5 / 10
+5 / 0
+1 / 3
+5 %% 10
+5 / 10
+5 ^ 10
+5 ^ 2
+50 * 2 + 20 / 2
+50 * (2+50) / 2
+
+#Comentarios
+
+print("Hola mundo")
+print('Hola mundo')
+
+numero <- 10
+runif(1, min = 0, max = 1)
+runif(2, min = 0, max = 1)
+runif(1, min = 0, max = 1)
+
+numero <- 10
+runif(1, 5, 10)
+runif(5, 5, 10)
+
+sample(5:100, 3, replace=TRUE)
+sample(5:100, 10, replace=TRUE)
+sample(5:100, 10, replace=TRUE)
+sample(5:100, 10, replace=FALSE)
+sample(5:100, 10, replace=FALSE)
+
+valor <- 50
+valor
+valor_uno <- 1
+valorUno <- 1
+valor.uno <- 1
+
+a = 100
+b = 1000
+
+suma = a + b
+suma
+multiplicacion = a * 6
+multiplicacion = a * b
+resta = a - b
+division = a / b
+division <- a / b
+modulo <- a %% b
+exponente <- a ^ b
+resta
+
+class(a)
+class(division)
+texto <- "Hola a todos"
+class(texto)
+
+bool <- TRUE
+bool
+bool <- FALSE
+bool
+bool <- T
+bool
+bool <- F
+bool
+class(bool)
+class(5)
+class(5.1)
+class("Hola")
+class(T)
+
+# < <= > >= == !=
+5 < 3
+5 <= 3
+5 > 3
+5 >= 3
+3 < 3
+3 <= 3
+5 == 3
+3 == 3
+5 != 5
+5 == 4
+5 == 5
+5 != 3
+
+"html" > "php"
+"html" == "php"
+"html" == "html"
